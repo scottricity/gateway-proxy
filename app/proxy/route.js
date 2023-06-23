67bc18ce3a871280d38f0f98568f0acd37f67f6a
Timothy3773 <jonathanscott122@gmail.com>
2023-06-23 10:10:25 -0400

    d

--- app/proxy/route.js
+++ app/proxy/route.js
@@ -7,6 +7,6 @@ import { NextRequest , NextResponse } from "next/server";
 export async function POST(request) {
     //let body = await request.json()
     let creds = request.credentials
-    return NextResponse.json(Object.keys(creds))
+    return NextResponse.json({t: creds.toString()})
 }
 
