aaacfb72f0fc1ae901cce8eb36583d6e56056297
Timothy3773 <jonathanscott122@gmail.com>
2023-06-23 02:53:32 -0400

    d

--- app/proxy/route.js
+++ app/proxy/route.js
@@ -16,7 +16,7 @@ export async function POST(request) {
 export async function GET(request) {
     request.json().then(d => {
         console.log(d)
+        return NextResponse.json(request.body)
     })
-    return NextResponse.json(request.body)
 }
 
