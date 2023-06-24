f8a920f37205e7afb51c0f0cb5b700b820e3f5c3
Timothy3773 <jonathanscott122@gmail.com>
2023-06-24 10:08:13 -0400

    if no body

--- app/proxy/route.js
+++ app/proxy/route.js
@@ -9,7 +9,7 @@ export async function POST(request) {
     let body = await request.json()
     if (!request.headers.has("url")) return NextResponse.json({msg: "missing url", status: 400});
     if (!request.headers.has("token")) return NextResponse.json({msg: "missing token", status: 401});
-    if (body == null || body == undefined || body == {}) return NextResponse.json({msg: "empty body", status: 400});
+    if (!body) return NextResponse.json({msg: "empty body", status: 400});
     return NextResponse.json({t: Object.keys(body)})
 }
 
