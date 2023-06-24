681454143ea1ec60f838bc25a52f1501e15ef845
Timothy3773 <jonathanscott122@gmail.com>
2023-06-24 09:33:44 -0400

    d

--- app/proxy/route.js
+++ app/proxy/route.js
@@ -5,12 +5,10 @@ import { NextRequest , NextResponse } from "next/server";
  * @param {NextRequest} request 
  */
 export async function POST(request) {
-    //let body = await request.json()
-    let creds = request.credentials
-    let t = []
-    request.headers.forEach((val, key, p) => {
-        t.push({[key]: val})
-    })
-    return NextResponse.json(t)
+    let body = await request.json()
+    if (!request.headers.has("url")) return NextResponse.json({msg: "missing url", status: 400});
+    if (!request.headers.has("token")) return NextResponse.json({msg: "missing token", status: 401});
+    if (body == null || body == undefined || body == {}) return NextResponse.json({msg: "empty body", status: 400});
+    return NextResponse.json(Object.keys(body))
 }
 
